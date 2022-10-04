e2399fa859475098519a22b12556487211b389b4
Ankur C <xs2ankur94@gmail.com>
2022-10-04 10:41:44 +0530

    added decoders

--- MyOwnArchitectureApp/ViewController.swift
+++ MyOwnArchitectureApp/ViewController.swift
@@ -5,25 +5,65 @@
 //  Created by Ankur Chaudhary on 03/10/22.
 //
 
+//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
+
 import UIKit
 
-struct Data: Codable {
-    var users: [User]
+struct RequestData: Codable {
+    var requests: [RequestDetail]?
 }
 
 struct User: Codable {
     var name: String
-    var tile: String
-    var image: String?
-    var created: Date?
-    var closedDate: Date?
+    var imageUrl: String?
     
-    enum CodingKeys: String {
-        case name = "UserName"
-        case title
-        case image = "userImage"
-        case created = "Created date"
-        case closedDate = "Closed date"
+    enum CodingKeys: String, CodingKey {
+        case name = "login"
+        case imageUrl = "avatar_url"
+    }
+}
+
+struct RequestDetail : Codable {
+    var uniqueId: Double
+    var title: String
+    var createdDate: Date?
+    var closedDate: Date?
+    var user: User
+    
+    enum CodingKeys: String, CodingKey {
+        case uniqueId = "id"
+        case title = "title"
+        case createdDate = "created_at"
+        case closedDate = "closed_at"
+        case user = "user"
+    }
+    
+    init(from decoder: Decoder) throws {
+        do {
+            let values = try decoder.container(keyedBy: CodingKeys.self)
+            uniqueId = try values.decode(Double.self, forKey: .uniqueId)
+            title = try values.decode(String.self, forKey: .title)
+            createdDate = try values.decode(Date.self, forKey: .createdDate)
+            closedDate = try values.decode(Date.self, forKey: .closedDate)
+            user = try values.decode(User.self, forKey: .user)
+        } catch  {
+//            debugPrint("Error While Decoding \(error)")
+            debugPrint("Error: ")
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        do {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(uniqueId, forKey: .uniqueId)
+        try container.encode(title, forKey: .title)
+        try container.encode(createdDate, forKey: .createdDate)
+        try container.encode(closedDate, forKey: .closedDate)
+        try container.encode(user, forKey: .user)
+        } catch  {
+//            debugPrint("Error While Decoding \(error)")
+            debugPrint("Error: ")
+        }
     }
 }
 
@@ -32,18 +72,19 @@ struct UserViewModel {
     
 }
 
-struct Observable<T> {
-    private var value: T? {
+class Observable<T> {
+    
+    var value: T? {
         didSet {
             listner?(value)
         }
     }
     
-    var listner: ((T?) ->())?
+    private var listner: ((T?) ->())?
     
-    mutating func bind(_ listner: @escaping ((T?) -> ())) {
-        self.listner = listner
+    func bind(_ listner: @escaping ((T?) -> ())) {
         listner(value)
+        self.listner = listner
     }
 
 }
